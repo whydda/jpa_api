54defeb6cad6911b493cbf6d74249d149ecb0583
whydda <whydda@naver.com>
2020-01-20 20:15:20 +0900

    JPA API 개발서버 구성

--- src/main/java/com/example/jpa_api/controller/CommonController.java
+++ src/main/java/com/example/jpa_api/controller/CommonController.java
@@ -3,11 +3,16 @@ package com.example.jpa_api.controller;
 import com.example.jpa_api.jpa.entity.Users;
 import com.example.jpa_api.jpa.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,10 +31,12 @@ public class CommonController {
         return "index"; // 4
     }
 
-    @PostMapping("/login")
-    public String login(@ModelAttribute Users users){
-        //등록
-        userService.create(users);
-        return "main";
+    @PostMapping("/test")
+    public ResponseEntity<Map<String, Object>> login(@ModelAttribute Users users){
+        Map<String, Object> params = new HashMap<>();
+        params.put("A", "A");
+        params.put("B", "B");
+        params.put("C", "C");
+        return new ResponseEntity<>(params, HttpStatus.OK);
     }
 }
--- src/main/java/com/example/jpa_api/core/WebSecurityConfig.java
+++ src/main/java/com/example/jpa_api/core/WebSecurityConfig.java
@@ -1,74 +1,25 @@
 package com.example.jpa_api.core;
 
-import com.example.jpa_api.jpa.service.UserDetailsServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	private UserDetailsServiceImpl userDetailsService;
-
-	@Autowired
-	public WebSecurityConfig(UserDetailsServiceImpl userDetailsService) {
-		this.userDetailsService = userDetailsService;
-	}
-
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-	}
-
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring()
-			.antMatchers(HttpMethod.OPTIONS, "*//**")
-			.antMatchers("/assets*//**")
-			.antMatchers("/css*//**")
-			.antMatchers("/fonts*//**")
-			.antMatchers("/img*//**")
-			.antMatchers("/js*//**")
-			.antMatchers("/hello.html")
-			.antMatchers("/register.html")
-			.antMatchers("/robots.txt");
+		web.ignoring().antMatchers("/static/css/**, /static/js/**, *.ico");		// Resources 파일이나 Javascript 파일 경로 무시
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
-
-		http.authorizeRequests().antMatchers("/", "/login", "/logout", "/users/register").permitAll();
-
-		http.authorizeRequests().anyRequest().access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
-
-		http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
-
-		// Config for Login Form
-		http.authorizeRequests().and().formLogin()//
-			// Submit URL of login page.
-			.loginProcessingUrl("/j_spring_security_check") // Submit URL
-			.loginPage("/login")//
-			.defaultSuccessUrl("/main", true)
-			.failureUrl("/login?error=true")//
-			.usernameParameter("username")//
-			.passwordParameter("password")
-			// Config for Logout Page
-			.and().logout().logoutUrl("/logout").logoutSuccessUrl("/");
-
-		http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/my-error-page");
+		http.authorizeRequests()
+				.anyRequest()
+				.permitAll();
 	}
 }
